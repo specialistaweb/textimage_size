9ffefc5adf9eed0c7d394d294cf54fd927a977c1
Alessandro Tuveri <at@specialistaweb.it>
2021-05-27 12:11:36 +0200

    First upload
    Initial version 1.2.3

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "textimage_size".
+ *
+ * Auto generated 26-04-2021 15:14
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Text Media/Image size',
+  'description' => 'Useful change the output size for standard bootstrap package output (column split 50% image - 50% text). For content types text/image (textpict), text/media and image',
+  'category' => 'plugin',
+  'author' => 'Alessandro Tuveri, Giulia Tuveri',
+  'author_email' => 'at@specialistaweb.it, gt@pecialistaweb.it',
+  'state' => 'beta',
+  'uploadfolder' => false,
+  'clearCacheOnLoad' => 0,
+  'version' => '1.2.3',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '9.5.0-10.99.99',
+      'bootstrap_package' => '9.0.0-11.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'clearcacheonload' => false,
+  'author_company' => NULL,
+);
+
---
+++ ext_localconf.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+
+// TCEFORM
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:textimage_size/Configuration/PageTS/TCEFORM.txt">');
+
+
+
