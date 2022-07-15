d506c817c0aa7987dd9a90355b3136bb1663cc5d
Alessandro Tuveri <at@specialistaweb.it>
2022-07-15 10:24:09 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
   'state' => 'beta',
   'uploadfolder' => false,
   'clearCacheOnLoad' => 0,
-  'version' => '2.0.0',
+  'version' => '2.0.1',
   'constraints' => 
   array (
     'depends' => 
