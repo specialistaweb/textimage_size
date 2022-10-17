9210b3c485844cb580bed531f1c70e7fec65f83b
Alessandro Tuveri <at@specialistaweb.it>
2022-10-17 10:46:32 +0200

    first implementation of SCSS for bootstrap5
    some things to review: grid-gutter-width and breakpoints

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,5 +4,7 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // add static typoscript includes
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript/Bootstrap_Package/bootstrap4/', 'TextImage Size: bootstrap4 with Scss');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript/BootstrapPackage/bootstrap5/', 'TextImage Size: bootstrap5 with Scss');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript/Bootstrap_Package/backward/', 'Text/Image size');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript/Bootstrap_Package/bootstrap4/', 'Text/Image size: Bootstrap4 (SCSS)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript/Bootstrap_Package/bootstrap5/', 'Text/Image size: Bootstrap5 (SCSS)');
+
