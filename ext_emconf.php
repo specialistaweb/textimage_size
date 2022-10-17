84e72c0da2601195e3de243d1f04ab4d9990c820
Alessandro Tuveri <at@specialistaweb.it>
2022-10-17 13:20:09 +0200

    backward compat with previous version

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
   'state' => 'beta',
   'uploadfolder' => false,
   'clearCacheOnLoad' => 0,
-  'version' => '2.1.0',
+  'version' => '2.1.1',
   'constraints' => 
   array (
     'depends' => 
