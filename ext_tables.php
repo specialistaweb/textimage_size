3c9780560d2f3c4bd74f1a58e666df11473f5f41
Alessandro Tuveri <at@specialistaweb.it>
2022-10-20 08:28:49 +0200

    minor changes
    can't activate the proper library after update, so that there is  non way to do this work (typo3 bug). Library is listed twice under web > template. Docs site will be updated to explain this behaviour.

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,7 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
+/*
 // no static file
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript', 'Text/Image size');
 
@@ -9,3 +10,4 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 // setup
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('textimage_size', 'setup', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:textimage_size/Configuration/TypoScript/setup.typoscript>"');
+*/
