cecbd0763502b109dc3e60a4d5a6fecf27aa3770
Alessandro Tuveri <at@specialistaweb.it>
2023-11-04 17:32:39 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '11.0.0-12.4.99,
+      'typo3' => '11.0.0-12.4.99',
       'bootstrap_package' => '12.0.0-14.0.9',
     ),
     'conflicts' => 
