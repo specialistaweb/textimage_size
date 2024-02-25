b19d1ebc2b7358fbe640497e3a212ecf9a130640
Alessandro Tuveri <at@specialistaweb.it>
2024-02-25 18:21:25 +0100

    drpped less support
    Since now only SCSS for bootstrap 5 is supported

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,12 +1,9 @@
 <?php
-/*
-if (!defined('TYPO3')) {
-    die('Access denied.');
-}
-*/
+
 
 // add static typoscript includes
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript/Bootstrap_Package/bootstrap4/', 'Text/Image size');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript/Bootstrap_Package/bootstrap4/', 'Text/Image size: Bootstrap4 (SCSS)');
+// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript/Bootstrap_Package/bootstrap4/', 'Text/Image size');
+// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript/Bootstrap_Package/bootstrap4/', 'Text/Image size: Bootstrap4 (SCSS)');
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript/Bootstrap_Package/bootstrap5/', 'Text/Image size: Bootstrap5 (SCSS)');
 
