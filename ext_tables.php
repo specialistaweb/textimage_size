56403882db71502f59b210b8d5a99de2631e55fe
Alessandro Tuveri <at@specialistaweb.it>
2024-02-25 18:19:29 +0100

    removed code commented

--- ext_tables.php
+++ ext_tables.php
@@ -1,13 +1,3 @@
 <?php
 defined('TYPO3') || die('Access denied.');
 
-/*
-// no static file
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('textimage_size', 'Configuration/TypoScript', 'Text/Image size');
-
-// constants
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('textimage_size', 'constants', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:textimage_size/Configuration/TypoScript/constants.typoscript>"');
-
-// setup
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('textimage_size', 'setup', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:textimage_size/Configuration/TypoScript/setup.typoscript>"');
-*/
